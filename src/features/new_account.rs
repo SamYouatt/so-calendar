54c2b990614b12b0f70fda00e6b7a1728388e3db
SamYouatt <sdyouatt@gmail.com>
2024-05-04 21:08:35 +0100

    exchange flow to get the auth bearer token

--- src/features/new_account.rs
+++ src/features/new_account.rs
@@ -1,4 +1,14 @@
-use oauth2::{basic::BasicClient, AuthUrl, ClientId, RedirectUrl, TokenUrl};
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
+
+use oauth2::{
+    basic::BasicClient, reqwest::http_client, AuthUrl, AuthorizationCode, ClientId, ClientSecret,
+    RedirectUrl, TokenUrl,
+};
+use url::Url;
 
 pub fn handle_new_account() {
     // Create the redirect url to show to the user
@@ -14,7 +24,9 @@ pub fn handle_new_account() {
         ClientId::new(
             "357015344564-7rf7b47n7add82k2t3hajfhq2pklthen.apps.googleusercontent.com".into(),
         ),
-        None,
+        Some(ClientSecret::new(
+            "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".into(),
+        )),
         auth_url,
         Some(token_url),
     )
@@ -32,4 +44,58 @@ pub fn handle_new_account() {
 
     println!("Open the link below in your browser to connect a Google account");
     println!("> {}", url);
+
+    // Create a tcp server to listen for the redirect response
+    let address = "localhost:42069";
+    let listener = TcpListener::bind(&address).expect("Failed to bind tcp listener");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        let result = handle_connection(stream, &address, &client);
+        println!("Result: {:?}", result);
+    }
+}
+
+fn handle_connection(
+    mut stream: TcpStream,
+    address: &str,
+    oauth_client: &BasicClient,
+) -> Result<(), NewAccountError> {
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let pieces: Vec<_> = request_line.split_whitespace().collect();
+
+    if pieces.len() != 3 || pieces[0] != "GET" || pieces[2] != "HTTP/1.1" {
+        return Err(NewAccountError::InvalidRedirectResponse);
+    }
+
+    let absolute_url = format!("{}/{}", address, pieces[1]);
+    let redirect_request_url =
+        Url::parse(&absolute_url).map_err(|_| NewAccountError::InvalidRedirectUrl)?;
+
+    let query_pairs: HashMap<_, _> = redirect_request_url.query_pairs().collect();
+
+    let auth_code = query_pairs
+        .get("code")
+        .map(|code| AuthorizationCode::new(code.to_string()))
+        .ok_or(NewAccountError::MissingAuthCode)?;
+
+    let auth_token = oauth_client
+        .exchange_code(auth_code)
+        .request(http_client)
+        .map_err(|e| {
+            println!("Error: {:?}", e);
+            NewAccountError::FailedTokenExchange
+        });
+
+    return Ok(());
+}
+
+#[derive(Debug)]
+enum NewAccountError {
+    InvalidRedirectResponse,
+    InvalidRedirectUrl,
+    MissingAuthCode,
+    FailedTokenExchange,
 }
