dc8c33cf62edfc64863f1a5343586f5dfc10ffcb
SamYouatt <sdyouatt@gmail.com>
2024-05-05 19:07:51 +0100

    refactor: extract client creation into function and use auth url method on client instead of building url manually also set up pkce challenge verfication

--- src/features/new_account.rs
+++ src/features/new_account.rs
@@ -7,7 +7,7 @@ use std::{
 use chrono::{DateTime, Utc};
 use oauth2::{
     basic::BasicClient, reqwest::http_client, AuthUrl, AuthorizationCode, ClientId, ClientSecret,
-    RedirectUrl, TokenResponse, TokenUrl,
+    CsrfToken, PkceCodeChallenge, PkceCodeVerifier, RedirectUrl, Scope, TokenResponse, TokenUrl,
 };
 use rusqlite::Connection;
 use serde::Deserialize;
@@ -28,16 +28,43 @@ struct UserProfile {
 }
 
 pub fn handle_new_account(application: &Application) {
-    // Create the redirect url to show to the user
+    let client = setup_oauth_client();
+
+    let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
+
+    let (auth_url, _) = client
+        .authorize_url(CsrfToken::new_random)
+        .add_scope(Scope::new("openid".into()))
+        .add_scope(Scope::new("email".into()))
+        .add_scope(Scope::new(
+            "https://www.googleapis.com/auth/calendar".into(),
+        ))
+        .set_pkce_challenge(pkce_challenge)
+        .url();
+
+    println!("Open the link below in your browser to connect a Google account");
+    println!("> {}", auth_url);
+
+    let address = "localhost:42069";
+    let listener = TcpListener::bind(&address).expect("Failed to bind tcp listener");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        // TODO: handle errors and exit
+        let _ = handle_tcp_request(stream, &address, &client, &application, pkce_verifier);
+        return;
+    }
+}
+
+fn setup_oauth_client() -> BasicClient {
     let auth_url_raw = String::from("https://accounts.google.com/o/oauth2/v2/auth");
     let auth_url = AuthUrl::new(auth_url_raw).expect("Invalid auth endpoint");
 
     let token_url_raw = String::from("https://www.googleapis.com/oauth2/v3/token");
     let token_url = TokenUrl::new(token_url_raw).expect("Invalid token endpoint");
 
-    let redirect_url = "http://localhost:42069/login";
-
-    let client = BasicClient::new(
+    BasicClient::new(
         ClientId::new(
             "357015344564-7rf7b47n7add82k2t3hajfhq2pklthen.apps.googleusercontent.com".into(),
         ),
@@ -47,39 +74,15 @@ pub fn handle_new_account(application: &Application) {
         auth_url,
         Some(token_url),
     )
-    .set_redirect_uri(RedirectUrl::new(redirect_url.into()).unwrap());
-
-    let url = format!(
-        "{}?response_type=code&client_id={}&scope=openid%20email%20https://www.googleapis.com/auth/calendar&redirect_uri={}",
-        "https://accounts.google.com/o/oauth2/v2/auth",
-        client.client_id().as_str(),
-        client
-            .redirect_url()
-            .expect("Couldn't find open id client redirect")
-            .as_str()
-    );
-
-    println!("Open the link below in your browser to connect a Google account");
-    println!("> {}", url);
-
-    // Create a tcp server to listen for the redirect response
-    let address = "localhost:42069";
-    let listener = TcpListener::bind(&address).expect("Failed to bind tcp listener");
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        // TODO: handle errors and exit
-        let _ = handle_connection(stream, &address, &client, &application);
-        return;
-    }
+    .set_redirect_uri(RedirectUrl::new("http://localhost:42069/auth/redirect".into()).unwrap())
 }
 
-fn handle_connection(
+fn handle_tcp_request(
     mut stream: TcpStream,
     address: &str,
     oauth_client: &BasicClient,
     application: &Application,
+    pkce_verifier: PkceCodeVerifier,
 ) -> Result<(), NewAccountError> {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
@@ -102,6 +105,7 @@ fn handle_connection(
 
     let auth_token = oauth_client
         .exchange_code(auth_code)
+        .set_pkce_verifier(pkce_verifier)
         .request(http_client)
         .map_err(|e| {
             println!("Error: {:?}", e);
