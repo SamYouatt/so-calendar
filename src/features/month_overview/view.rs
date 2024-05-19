d5a1898748536c65440753227c14bda552180d1f
SamYouatt <sdyouatt@gmail.com>
2024-05-19 20:27:00 +0100

    feat: basic layout for month overview with month box on the left and today overview on the right

--- src/features/month_overview/view.rs
+++ src/features/month_overview/view.rs
@@ -1,12 +1,29 @@
+use ratatui::prelude::*;
 use ratatui::symbols::border;
 use ratatui::widgets::*;
-use ratatui::prelude::*;
 
 pub fn render(frame: &mut Frame) {
-    let block = Block::default()
+    let main_layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(75), Constraint::Min(22)])
+        .split(frame.size());
+
+    let calendar_block = Block::default()
+        .title("SoCalendar")
         .borders(Borders::ALL)
         .border_set(border::THICK);
-    let widget = Paragraph::new("SoCalendar").centered().block(block);
+    let month_view_placeholder = Paragraph::new("Month view")
+        .centered()
+        .block(calendar_block);
 
-    frame.render_widget(widget, frame.size());
+    let today_block = Block::default()
+        .title("19-May")
+        .borders(Borders::ALL)
+        .border_set(border::THICK);
+    let today_view_placeholder = Paragraph::new("Today overview")
+        .centered()
+        .block(today_block);
+
+    frame.render_widget(month_view_placeholder, main_layout[0]);
+    frame.render_widget(today_view_placeholder, main_layout[1]);
 }
