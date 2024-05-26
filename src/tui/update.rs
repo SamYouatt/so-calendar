038afbe0abec9f9e0147871ae04a2b87f4fcdd98
SamYouatt <sdyouatt@gmail.com>
2024-05-26 12:19:58 +0100

    refactor: separate universal and page sepcific event handling

--- src/tui/update.rs
+++ src/tui/update.rs
@@ -1,17 +1,20 @@
 use super::model::{CurrentState, Message, Model};
 
 pub fn update(model: &mut Model, msg: Message) -> Option<Message> {
+    // Handle any universal actions
     match msg {
         Message::ManageAccounts => model.current_state = CurrentState::Account,
         Message::Quit => model.current_state = CurrentState::Done,
-        Message::New => match model.current_state {
-            CurrentState::Account => model.current_state = CurrentState::SignUpOptions,
-            _ => {}
-        },
-        Message::LoginStarted => todo!(),
-        Message::LoginSuccess => todo!(),
-        Message::Back => todo!(),
+        _ => {}
     };
+
+    // Handle page specific actions
+    match model.current_state {
+        CurrentState::Account if msg == Message::New => {
+            model.current_state = CurrentState::SignUpOptions
+        }
+        _ => {}
+    }
 
     None
 }
