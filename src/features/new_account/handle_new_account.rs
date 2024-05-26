79a2293d29fdce1989842835ae14f846b1533e33
SamYouatt <sdyouatt@gmail.com>
2024-05-26 13:07:35 +0100

    remove: all the old interactive list selection stuff cli arg version is now a far future problem to solve

--- src/features/new_account/handle_new_account.rs
+++ src/features/new_account/handle_new_account.rs
@@ -12,8 +12,6 @@ use url::Url;
 
 use crate::{features::new_account::tcp_request_handler::handle_tcp_request, Application};
 
-use super::url_tui::{init_terminal, restore_terminal, update, view, Message, Model, RunningState};
-
 pub struct Account {
     pub access_token: String,
     pub refresh_token: String,
@@ -39,11 +37,6 @@ pub fn handle_new_account(application: &Application) -> Result<()> {
         ))
         .set_pkce_challenge(pkce_challenge)
         .url();
-
-    // TODO: handle failure to open browser or clipboard by presenting plain link
-    interactive_auth_prompt(auth_url)?;
-
-    println!("Waiting for you to log in...");
 
     let address = "localhost:42069";
     let listener = TcpListener::bind(address).expect("Failed to bind tcp listener");
@@ -74,64 +67,4 @@ enum InteractionError {
     FailedCopyToClipboard(String),
     #[error("Unexpected error during interactive terminal")]
     UnexpectedError(#[from] eyre::Error),
-}
-
-fn interactive_auth_prompt(auth_url: Url) -> Result<(), InteractionError> {
-    let mut terminal = init_terminal()?;
-
-    let mut model = Model::new();
-
-    while model.state == RunningState::Running {
-        terminal.draw(|frame| view(&mut model, frame))?;
-
-        if let Some(message) = handle_event().wrap_err("Unable to read terminal input")? {
-            update(&mut model, message);
-        }
-    }
-
-    match model.state {
-        RunningState::SelectionMade(selection) => {
-            restore_terminal(&mut terminal)?;
-
-            match selection {
-                super::url_tui::LoginOption::OpenBrowser => {
-                    open::that(auth_url.as_str())?;
-                }
-                super::url_tui::LoginOption::CopyToClipboard => {
-                    let mut clipboard = ClipboardContext::new()
-                        .map_err(|e| InteractionError::FailedCopyToClipboard(e.to_string()))?;
-                    clipboard
-                        .set_contents(auth_url.into())
-                        .map_err(|e| InteractionError::FailedCopyToClipboard(e.to_string()))?;
-                }
-            };
-
-            Ok(())
-        }
-        RunningState::Exited => {
-            restore_terminal(&mut terminal)?;
-            std::process::exit(0);
-        }
-        RunningState::Running => unreachable!(),
-    }
-}
-
-fn handle_event() -> Result<Option<Message>> {
-    if let Event::Key(key) = event::read()? {
-        if key.kind == KeyEventKind::Press {
-            return Ok(handle_key(key));
-        }
-    }
-
-    Ok(None)
-}
-
-fn handle_key(key: event::KeyEvent) -> Option<Message> {
-    match key.code {
-        KeyCode::Up => Some(Message::Previous),
-        KeyCode::Down => Some(Message::Next),
-        KeyCode::Enter => Some(Message::Select),
-        KeyCode::Esc => Some(Message::Quit),
-        _ => None,
-    }
 }
