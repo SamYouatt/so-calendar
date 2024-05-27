a928e44cafe89b0cee6bfdaca6029f701f02d889
SamYouatt <sdyouatt@gmail.com>
2024-05-27 18:55:11 +0100

    add keybinding for manage calendars message

--- src/tui/view.rs
+++ src/tui/view.rs
@@ -8,8 +8,10 @@ use super::model::Model;
 pub fn view(model: &Model, frame: &mut Frame) {
     match &model.current_state {
         CurrentState::Done => {}
+
         CurrentState::MonthView => features::month_overview::view::render(frame),
         CurrentState::DaysView => features::days_view::view::render(frame),
+
         CurrentState::Account(accounts) => {
             features::account_overview::view::render(accounts, frame, &model.application)
         }
@@ -23,5 +25,7 @@ pub fn view(model: &Model, frame: &mut Frame) {
         CurrentState::PendingLogin(_) => {
             features::new_account::view::render_waiting_for_signin(frame, &model.application)
         }
+
+        CurrentState::Calendars => todo!(),
     };
 }
