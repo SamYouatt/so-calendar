29f1e775a383c6ddbcd094fe91e973479006cde0
SamYouatt <sdyouatt@gmail.com>
2024-05-29 21:20:35 +0100

    turn the calendar resources into internal calendar model

--- src/features/new_account/populate_new_calendars.rs
+++ src/features/new_account/populate_new_calendars.rs
@@ -18,13 +18,44 @@ struct CalendarResource {
     description: Option<String>,
 }
 
+struct Calendar {
+    id: String,
+    primary_calendar: bool,
+    title: String,
+    description: Option<String>,
+}
+
+impl From<CalendarResource> for Calendar {
+    fn from(calendar: CalendarResource) -> Self {
+        Self {
+            id: calendar.id,
+            primary_calendar: calendar.primary.unwrap_or(false),
+            title: calendar.summary,
+            description: calendar.description,
+        }
+    }
+}
+
 pub async fn populate_new_calendars(email: String, application: &Application) -> Result<()> {
     let http_client = reqwest::Client::new();
     let calendar_list_request =
         http_client.get("https://www.googleapis.com/calendar/v3/users/me/calendarList");
-    let response = application.google_client.send(email, calendar_list_request).await?;
+
+    let response = application
+        .google_client
+        .send(email, calendar_list_request)
+        .await?;
 
     let calendar_list: CalendarListResponse = response.json().await?;
+    let calendars: Vec<Calendar> = calendar_list
+        .items
+        .into_iter()
+        .map(|cal| Calendar::from(cal))
+        .collect();
 
+    store_calendars(&calendars, application).await
+}
+
+async fn store_calendars(calendars: &[Calendar], application: &Application) -> Result<()> {
     todo!()
 }
