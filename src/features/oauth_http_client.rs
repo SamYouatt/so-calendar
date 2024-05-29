a7496abc2c16f044785c470b2b91048d4c0d44cd
SamYouatt <sdyouatt@gmail.com>
2024-05-29 21:57:38 +0100

    make oauth client account id based not email

--- src/features/oauth_http_client.rs
+++ src/features/oauth_http_client.rs
@@ -4,6 +4,7 @@ use oauth2::{basic::BasicClient, reqwest::http_client, RefreshToken, TokenRespon
 use reqwest::RequestBuilder;
 use sqlx::{query, SqlitePool};
 use thiserror::Error;
+use uuid::Uuid;
 
 #[derive(Clone)]
 pub struct GoogleOAuthClient {
@@ -38,12 +39,14 @@ impl GoogleOAuthClient {
 
     pub async fn send(
         &self,
-        account_email: String,
+        account_id: Uuid,
         request_builder: RequestBuilder,
     ) -> Result<reqwest::Response, OAuthHttpClientError> {
+        let account_id_string = account_id.to_string();
+
         let token_details = match query!(
-            "SELECT access_token, refresh_token, expires_at FROM accounts WHERE email = $1 LIMIT 1",
-            account_email
+            "SELECT access_token, refresh_token, expires_at FROM accounts WHERE id = $1 LIMIT 1",
+            account_id_string
         )
         .fetch_one(&self.db)
         .await
@@ -61,7 +64,7 @@ impl GoogleOAuthClient {
                 }
             }
             Err(sqlx::Error::RowNotFound) => {
-                return Err(OAuthHttpClientError::NoAccount(account_email))
+                return Err(OAuthHttpClientError::NoAccount(account_id.to_string()))
             }
             Err(e) => return Err(e.into()),
         };
@@ -82,14 +85,12 @@ impl GoogleOAuthClient {
                     .expires_in()
                     .unwrap_or(std::time::Duration::from_secs(3600));
 
-            upsert_access_token_details(&self.db, &new_access_token, new_expiry, &account_email).await?;
+            upsert_access_token_details(&self.db, &new_access_token, new_expiry, account_id).await?;
 
             new_access_token
         } else {
             token_details.access_token
         };
-
-        // Step 4: store the new access token and expiration date if there was one
 
         let result = request_builder.bearer_auth(access_token).send().await?;
         Ok(result)
@@ -100,15 +101,16 @@ async fn upsert_access_token_details(
     db: &SqlitePool,
     access_token: &str,
     expires_at: DateTime<Utc>,
-    account_email: &str,
+    account_id: Uuid,
 ) -> Result<(), OAuthHttpClientError> {
     let expires_at_string = expires_at.to_rfc3339();
+    let account_id = account_id.to_string();
 
     query!(
-        "UPDATE accounts SET access_token = $1, expires_at = $2 WHERE email = $3",
+        "UPDATE accounts SET access_token = $1, expires_at = $2 WHERE id = $3",
         access_token,
         expires_at_string,
-        account_email,
+        account_id,
     )
     .execute(db)
     .await?;
