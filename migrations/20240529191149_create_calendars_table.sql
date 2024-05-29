c3b609d54ffc7f690a28173e3b64cfce68816a16
SamYouatt <sdyouatt@gmail.com>
2024-05-29 20:58:11 +0100

    fix: primary is reserved keyword in sql so rename column to primary_cal

--- migrations/20240529191149_create_calendars_table.sql
+++ migrations/20240529191149_create_calendars_table.sql
@@ -2,6 +2,6 @@
 CREATE TABLE IF NOT EXISTS calendars(
                     id TEXT PRIMARY KEY,
                     calendar_id TEXT NOT NULL UNIQUE,
-                    primary INTEGER NOT NULL,
+                    primary_calendar INTEGER NOT NULL,
                     title TEXT NOT NULL,
                     description TEXT)
