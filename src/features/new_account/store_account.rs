67b3636cbe36f776c44514d83ee3bd94b13a3ae3
SamYouatt <sdyouatt@gmail.com>
2024-05-29 19:14:23 +0100

    impl: creating account should insert uuid as primary key now previously was just auto integer but you could have calendars etc linked to a number of an accoun that is later deleted and you would have problems of collisions. Sqlite doesn't support uuid as a data type though so have to be stored as a string

--- src/features/new_account/store_account.rs
+++ src/features/new_account/store_account.rs
@@ -1,5 +1,6 @@
 use color_eyre::eyre::Result;
 use sqlx::query;
+use uuid::Uuid;
 
 use crate::configuration::Application;
 
@@ -7,10 +8,11 @@ use super::handle_new_account::Account;
 
 pub async fn store_account(account: Account, application: &Application) -> Result<()> {
     let expiry_as_string = account.expiry.to_rfc3339();
+    let account_id = Uuid::new_v4().to_string();
 
     query!(
-        "INSERT INTO accounts (email, access_token, refresh_token, expires_at) VALUES ($1, $2, $3, $4)",
-        account.email, account.access_token, account.refresh_token, expiry_as_string
+        "INSERT INTO accounts (id, email, access_token, refresh_token, expires_at) VALUES ($1, $2, $3, $4, $5)",
+        account_id, account.email, account.access_token, account.refresh_token, expiry_as_string
         ).execute(&application.db).await?;
 
     Ok(())
