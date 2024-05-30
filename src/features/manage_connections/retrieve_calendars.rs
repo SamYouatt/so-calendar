bbac01269329afcf26715428a74de694375de05a
SamYouatt <sdyouatt@gmail.com>
2024-05-30 21:22:46 +0100

    impl: retrieve calendars from the calendars table

---
+++ src/features/manage_connections/retrieve_calendars.rs
@@ -0,0 +1,12 @@
+use color_eyre::eyre::Result;
+use sqlx::SqlitePool;
+
+use super::Calendar;
+
+async fn retrieve_calendars(db: &SqlitePool) -> Result<Vec<Calendar>> {
+    let calendars: Vec<Calendar> = sqlx::query_as!(Calendar, r#"SELECT id as "id:uuid::Uuid", account_id as "account_id: uuid::Uuid", title, description FROM calendars"#)
+        .fetch_all(db)
+        .await?;
+
+    Ok(calendars)
+}
