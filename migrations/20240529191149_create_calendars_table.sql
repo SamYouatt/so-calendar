52cb3478a6bd956834fef05d68a88c4c92152f56
SamYouatt <sdyouatt@gmail.com>
2024-05-30 21:22:17 +0100

    migrations: primary key id on calendars shouldn't be null

--- migrations/20240529191149_create_calendars_table.sql
+++ migrations/20240529191149_create_calendars_table.sql
@@ -1,6 +1,6 @@
 -- Create calendars table
 CREATE TABLE IF NOT EXISTS calendars(
-                    id TEXT PRIMARY KEY,
+                    id TEXT PRIMARY KEY NOT NULL,
                     account_id TEXT NOT NULL REFERENCES accounts (id),
                     calendar_id TEXT NOT NULL UNIQUE,
                     primary_calendar INTEGER NOT NULL,
