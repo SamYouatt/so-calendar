c9f035243a81d76ca2638e746bdc6c7f9e08586b
SamYouatt <sdyouatt@gmail.com>
2024-05-31 09:06:02 +0100

    migrations: move to integer primary key instead of uuid

--- migrations/20240529191149_create_calendars_table.sql
+++ migrations/20240529191149_create_calendars_table.sql
@@ -1,7 +1,7 @@
 -- Create calendars table
 CREATE TABLE IF NOT EXISTS calendars(
-                    id TEXT PRIMARY KEY NOT NULL,
-                    account_id TEXT NOT NULL REFERENCES accounts (id),
+                    id INTEGER PRIMARY KEY NOT NULL,
+                    account_id INTEGER NOT NULL REFERENCES accounts (id),
                     calendar_id TEXT NOT NULL UNIQUE,
                     primary_calendar INTEGER NOT NULL,
                     title TEXT NOT NULL,
