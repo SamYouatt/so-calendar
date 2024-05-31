2b9de4298795200957ee43495f735480300b2f73
SamYouatt <sdyouatt@gmail.com>
2024-05-31 17:27:05 +0100

    impl: only show the calendars for the currently selected account

--- src/features/manage_connections/manage_connections_view.rs
+++ src/features/manage_connections/manage_connections_view.rs
@@ -1,19 +1,22 @@
-use std::fmt::Display;
-
 use ratatui::prelude::*;
 use ratatui::widgets::block::Title;
 use ratatui::widgets::*;
 
 use super::manage_connections_state::ManageConnectionsState;
-use super::Account;
-
-impl Display for Account {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.email)
-    }
-}
 
 pub fn render(state: &ManageConnectionsState, frame: &mut Frame<'_>) {
+    let selected_account = &state.accounts[state.selected_account_index];
+
+    let accounts = state.accounts.iter().map(|acc| acc.email.to_string());
+    let filtered_calendars = state
+        .calendars
+        .iter()
+        .filter(|cal| cal.account_id == selected_account.id)
+        .map(|cal| cal.title.to_string());
+
+    let mut account_list_state = ListState::default();
+    account_list_state.select(Some(state.selected_account_index));
+
     let vertical_split =
         Layout::horizontal([Constraint::Percentage(50), Constraint::Percentage(50)])
             .split(frame.size());
@@ -26,20 +29,15 @@ pub fn render(state: &ManageConnectionsState, frame: &mut Frame<'_>) {
         .border_type(BorderType::Thick)
         .title(Title::from("Calendars").alignment(Alignment::Left));
 
-    let account_list = List::new(state.accounts.iter().map(|acc| acc.email.to_string()))
+    let account_list = List::new(accounts)
         .block(accounts_block)
-        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
         .highlight_symbol(">>")
         .direction(ListDirection::TopToBottom);
 
-    let calendar_list = List::new(state.calendars.iter().map(|cal| cal.title.to_string()))
+    let calendar_list = List::new(filtered_calendars)
         .block(calendars_block)
-        .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
         .highlight_symbol(">>")
         .direction(ListDirection::TopToBottom);
-
-    let mut account_list_state = ListState::default();
-    account_list_state.select(Some(state.selected_account_index));
 
     frame.render_stateful_widget(account_list, vertical_split[0], &mut account_list_state);
     frame.render_widget(calendar_list, vertical_split[1]);
