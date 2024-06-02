bfe761a251d15a8e9473252c3baaa50edace2f6e
SamYouatt <sdyouatt@gmail.com>
2024-06-02 16:19:53 +0100

    wip: nail down more of the representation of the event api spec

--- src/features/days_view/handle_days_view_message.rs
+++ src/features/days_view/handle_days_view_message.rs
@@ -1,4 +1,4 @@
-use chrono::{DateTime, Duration, Local, NaiveTime, Utc};
+use chrono::{DateTime, Duration, Local, NaiveDate, NaiveTime, Utc};
 use color_eyre::eyre::Result;
 use serde::Deserialize;
 
@@ -12,36 +12,89 @@ use crate::{
 
 use super::retrieve_calendars::Calendar;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
 struct EventListResponse {
     items: Vec<EventResource>,
 }
 
-#[derive(Deserialize)]
-struct EventResource {
+#[derive(Deserialize, Debug)]
+#[serde(tag = "status")]
+pub enum EventResource {
+    #[serde(rename = "confirmed")]
+    Confirmed(ConfirmedEvent),
+    #[serde(rename = "tentative")]
+    Tentative(ConfirmedEvent),
+    #[serde(rename = "cancelled")]
+    Cancelled(CancelledEventResource),
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(untagged)]
+enum ConfirmedEvent {
+    Event(ConfirmedEventResource),
+    DayEvent(ConfirmedDayEventResource),
+}
+
+#[derive(Deserialize, Debug)]
+struct ConfirmedEventResource {
+    id: String,
     summary: String,
     description: Option<String>,
     start: DateObject,
     end: DateObject,
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Debug)]
+struct ConfirmedDayEventResource {
+    id: String,
+    summary: String,
+    description: Option<String>,
+    start: DayDateObject,
+    end: DayDateObject,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct CancelledEventResource {
+    id: String,
+}
+
+#[derive(Deserialize, Debug)]
 struct DateObject {
     #[serde(rename = "dateTime")]
     date_time: DateTime<Utc>,
 }
 
+#[derive(Deserialize, Debug)]
+struct DayDateObject {
+    date: NaiveDate,
+}
+
 #[derive(Debug)]
 pub struct Event {
+    id: String,
     title: String,
     description: Option<String>,
     start_time: DateTime<Utc>,
     end_time: DateTime<Utc>,
 }
 
-impl From<EventResource> for Event {
-    fn from(event_resource: EventResource) -> Self {
+#[derive(Debug)]
+pub struct DayEvent {
+    id: String,
+    title: String,
+    description: Option<String>,
+    date: NaiveDate,
+}
+
+#[derive(Debug)]
+pub struct CancelledEvent {
+    id: String,
+}
+
+impl From<ConfirmedEventResource> for Event {
+    fn from(event_resource: ConfirmedEventResource) -> Self {
         Event {
+            id: event_resource.id,
             title: event_resource.summary,
             description: event_resource.description,
             start_time: event_resource.start.date_time,
@@ -60,6 +113,7 @@ pub async fn handle_load_days_view(model: &mut Model) -> Result<()> {
         all_events.append(&mut events);
     }
 
+    println!("{:?}", all_events);
     model.current_state = CurrentState::DaysView(DaysViewState { events: all_events });
     Ok(())
 }
@@ -70,7 +124,7 @@ async fn retrieve_calendar_events(
 ) -> Result<Vec<Event>> {
     let now = Local::now();
     let today_midnight = now.with_time(NaiveTime::MIN).unwrap();
-    let tomorrow_midnight = today_midnight + Duration::days(1);
+    let tomorrow_midnight = today_midnight + Duration::days(2);
 
     let events_starting_from = today_midnight.to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
     let events_ending_at = tomorrow_midnight.to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
@@ -90,14 +144,47 @@ async fn retrieve_calendar_events(
         .send(calendar.account_id, calendar_list_request)
         .await?;
 
+    //println!("{:?}", response.bytes().await.unwrap());
+    //Ok(vec![])
+
     // TODO: handle non ok responses
 
     let event_list: EventListResponse = response.json().await?;
-    let events: Vec<Event> = event_list
-        .items
-        .into_iter()
-        .map(|event| Event::from(event))
-        .collect();
+    println!("{:#?}", event_list);
+    Ok(vec![])
+    //let events: Vec<Event> = event_list
+    //    .items
+    //    .into_iter()
+    //    .map(|event| Event::from(event))
+    //    .collect();
+    //
+    //Ok(events)
+}
 
-    Ok(events)
+#[cfg(test)]
+mod test {
+    use crate::{
+        configuration::Application,
+        features::days_view::{
+            handle_days_view_message::retrieve_calendar_events, retrieve_calendars::Calendar,
+        },
+    };
+
+    #[tokio::test]
+    async fn test_google_events() {
+        let application = Application::setup().await.unwrap();
+
+        let calendar = Calendar {
+            account_id: 2,
+            calendar_id: "sam.youatt@gearset.com".to_string(),
+        };
+
+        let result = retrieve_calendar_events(calendar, &application.google_client)
+            .await
+            .unwrap();
+
+        println!("{:#?}", result);
+
+        assert!(false);
+    }
 }
