7af296bb10629d8f05a41e0dcfb8072ec08ea32e
SamYouatt <sdyouatt@gmail.com>
2024-06-10 21:33:29 +0100

    render the current date at the top of the today panel

--- src/features/month_view/view.rs
+++ src/features/month_view/view.rs
@@ -1,20 +1,41 @@
+use chrono::{Local, NaiveDate};
 use ratatui::prelude::*;
 use ratatui::style::palette::tailwind;
 use ratatui::symbols::border;
 use ratatui::widgets::*;
 
+struct TodayWidget {
+    date: NaiveDate,
+}
+
+impl Widget for TodayWidget {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+        let today_block = Block::default()
+            .padding(Padding::left(1))
+            .style(Style::new().bg(tailwind::STONE.c100));
+
+        let formatted_date = self.date.format("%d %B, %A").to_string();
+        Paragraph::new(formatted_date)
+            .block(today_block)
+            .style(Style::new().fg(tailwind::RED.c500).bold())
+            .render(area, buf);
+    }
+}
+
 pub fn render(frame: &mut Frame) {
     let main_layout =
-        Layout::horizontal([Constraint::Fill(1), Constraint::Length(30)]).split(frame.size());
+        Layout::horizontal([Constraint::Fill(3), Constraint::Fill(1)]).split(frame.size());
 
     let month_block = Block::default().style(Style::new().bg(tailwind::STONE.c200));
     let month_view_placeholder = Paragraph::new("month view").block(month_block);
 
-    let today_block = Block::default().style(Style::new().bg(tailwind::STONE.c100));
-    let today_view_placeholder = Paragraph::new("Today overview")
-        .centered()
-        .block(today_block);
+    let today_widget = TodayWidget {
+        date: Local::now().date_naive(),
+    };
 
     frame.render_widget(month_view_placeholder, main_layout[0]);
-    frame.render_widget(today_view_placeholder, main_layout[1]);
+    frame.render_widget(today_widget, main_layout[1]);
 }
