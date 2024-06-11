7c49b08243eb9fec15403c7be5b43edebe3a93df
SamYouatt <sdyouatt@gmail.com>
2024-06-11 18:00:05 +0100

    add utility for truncating text with … on the end

--- src/features/month_view/view.rs
+++ src/features/month_view/view.rs
@@ -2,11 +2,19 @@ use chrono::{Local, NaiveDate};
 use ratatui::layout::Flex;
 use ratatui::prelude::*;
 use ratatui::style::palette::tailwind;
-use ratatui::symbols::border;
 use ratatui::widgets::*;
 
 use crate::domain::events::{DayEvent, Event};
 use crate::tui::model::EventsState;
+
+/// Example: "really long" -> "really l…"
+fn truncate_text(text: &str, length: usize) -> String {
+    if text.len() < length {
+        return text.to_string();
+    }
+
+    format!("{}…", &text[..length-1].trim())
+}
 
 struct EventWidget<'a> {
     event: &'a Event,
@@ -39,20 +47,16 @@ impl Widget for EventWidget<'_> {
         // Main event information
         let event_content_layout = Layout::vertical([Constraint::Length(1), Constraint::Length(1)])
             .split(event_row_layout[1]);
+        let max_width: usize = event_content_layout[1].width.into();
 
-        Paragraph::new(self.event.title.to_string())
+        let event_title = truncate_text(&self.event.title, max_width);
+        Paragraph::new(event_title)
             .style(Style::new().fg(tailwind::STONE.c800))
             .render(event_content_layout[0], buf);
 
         if let Some(description) = &self.event.description {
-            let mut description = description.to_owned();
-            let max_length: usize = event_content_layout[1].width.into();
 
-            if description.len() >= max_length {
-                description = format!("{}…", &description[..max_length - 1].trim());
-            }
-
-            Paragraph::new(description.as_str())
+            Paragraph::new(truncate_text(description, max_width))
                 .style(Style::new().fg(tailwind::STONE.c500))
                 .render(event_content_layout[1], buf);
         }
@@ -101,13 +105,16 @@ impl Widget for DayEventWidget<'_> {
             .render(event_row_layout[0], buf);
 
         // Main event information
-        Paragraph::new(self.event.title.to_string())
+        let max_width: usize = event_row_layout[1].width.into();
+
+        let event_title = truncate_text(&self.event.title, max_width);
+        Paragraph::new(event_title)
             .style(Style::new().fg(tailwind::STONE.c800))
             .render(event_row_layout[1], buf);
 
         // All day indicator
         Paragraph::new("All day")
-            .style(Style::new().fg(tailwind::STONE.c600))
+            .style(Style::new().fg(tailwind::STONE.c500))
             .render(event_row_layout[2], buf);
     }
 }
