711af50e2be217cb0a53aed8366a0f3bfa6eb206
SamYouatt <sdyouatt@gmail.com>
2024-06-12 21:02:58 +0100

    fix error hook installation firstly I was calling the default eyre hooks straight after setting up custom ones, and also wasn't restoring the terminal on eyre hoook, only the normal panic hook

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
-use std::{io::stdout, panic};
+use std::panic;
 
 use color_eyre::eyre::Result;
-use crossterm::{terminal::{disable_raw_mode, LeaveAlternateScreen}, ExecutableCommand};
-use socal::{configuration::Application, run};
+use socal::{configuration::Application, run, tui::restore_terminal};
 
 // Ensure terminal is reset properly upon panic
-fn install_panic_hook() {
-    let original_hook = panic::take_hook();
+fn install_panic_hook() -> Result<()> {
+    let hook_builder = color_eyre::config::HookBuilder::default();
+    let (panic_hook, eyre_hook) = hook_builder.into_hooks();
+
+    let panic_hook = panic_hook.into_panic_hook();
     panic::set_hook(Box::new(move |panic_info| {
-        stdout().execute(LeaveAlternateScreen).unwrap();
-        disable_raw_mode().unwrap();
-        original_hook(panic_info);
+        restore_terminal().expect("Failed to restore terminal. Run `reset` or restart terminal.");
+        panic_hook(panic_info);
     }));
+
+    let eyre_hook = eyre_hook.into_eyre_hook();
+    eyre::set_hook(Box::new(move |error| {
+        restore_terminal().expect("Failed to restore terminal. Run `reset` or restart terminal.");
+        eyre_hook(error)
+    }))?;
+
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    install_panic_hook();
-    color_eyre::install()?;
+    install_panic_hook()?;
 
     let application = Application::setup().await?;
 
     run(application).await?;
+
+    restore_terminal()?;
 
     Ok(())
 }
