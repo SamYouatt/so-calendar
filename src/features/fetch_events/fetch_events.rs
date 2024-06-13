314e0b141e8f858d9528633b70bb5523c57f9f27
SamYouatt <sdyouatt@gmail.com>
2024-06-13 21:32:54 +0100

    tweak fetch events function to accept start and end date time gives more flexibility to users of the function than a date and a duration

--- src/features/fetch_events/fetch_events.rs
+++ src/features/fetch_events/fetch_events.rs
@@ -1,4 +1,4 @@
-use chrono::{Duration, Local, NaiveDate, NaiveTime};
+use chrono::{DateTime, Local};
 use color_eyre::eyre::Result;
 use eyre::Context;
 use sqlx::SqlitePool;
@@ -16,14 +16,14 @@ use super::deserialise_event_response::deserialise_event_list_response;
 
 /// Begin a fetch of calendar events which will send a message with
 /// the results, or an error message
-/// start_date: inclusive start date at midnight
-/// end_date: exclusive start date - events up to this date at midnight
-pub fn run_fetch_events_task(start_date: NaiveDate, end_date: NaiveDate, model: &Model) {
+/// start_time: start time to fetch events
+/// end_time: exclusive end time to fetch events
+pub fn run_fetch_events_task(start_time: DateTime<Local>, end_time: DateTime<Local>, model: &Model) {
     let db = model.application.db.clone();
     let google_client = model.application.google_client.clone();
 
     tokio::spawn(async move {
-        match fetch_events(start_date, end_date, db, google_client).await {
+        match fetch_events(start_time, end_time, db, google_client).await {
             // Send finished event with payload
             Ok(_) => todo!(),
             // Create error message event with user facing error
@@ -33,8 +33,8 @@ pub fn run_fetch_events_task(start_date: NaiveDate, end_date: NaiveDate, model: 
 }
 
 async fn fetch_events(
-    start_date: NaiveDate,
-    end_date: NaiveDate,
+    start_time: DateTime<Local>,
+    end_time: DateTime<Local>,
     db: SqlitePool,
     google_client: GoogleOAuthClient,
 ) -> Result<(Vec<Event>, Vec<DayEvent>)> {
@@ -43,9 +43,10 @@ async fn fetch_events(
     let mut all_events = vec![];
     let mut all_day_events = vec![];
     for calendar in calendars.into_iter() {
+        // TODO: make these all run in parallel
         // TODO: some of these might fail
         let (mut events, mut day_events) =
-            retrieve_calendar_events(calendar, &google_client).await?;
+            retrieve_calendar_events(start_time, end_time, calendar, &google_client).await?;
         all_events.append(&mut events);
         all_day_events.append(&mut day_events);
     }
@@ -61,15 +62,13 @@ async fn retrieve_calendars(db: &SqlitePool) -> Result<Vec<Calendar>> {
 }
 
 async fn retrieve_calendar_events(
+    start_time: DateTime<Local>,
+    end_time: DateTime<Local>,
     calendar: Calendar,
     google_client: &GoogleOAuthClient,
 ) -> Result<(Vec<Event>, Vec<DayEvent>)> {
-    let now = Local::now();
-    let today_midnight = now.with_time(NaiveTime::MIN).unwrap();
-    let tomorrow_midnight = today_midnight + Duration::days(2);
-
-    let events_starting_from = today_midnight.to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
-    let events_ending_at = tomorrow_midnight.to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
+    let events_from = start_time.to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
+    let events_until = end_time.to_rfc3339_opts(chrono::SecondsFormat::Secs, true);
 
     let events_list_url = format!(
         "https://www.googleapis.com/calendar/v3/calendars/{}/events",
@@ -78,8 +77,8 @@ async fn retrieve_calendar_events(
 
     let http_client = reqwest::Client::new();
     let calendar_list_request = http_client.get(events_list_url.to_string()).query(&[
-        ("timeMin", events_starting_from.as_str()),
-        ("timeMax", events_ending_at.as_str()),
+        ("timeMin", events_from.as_str()),
+        ("timeMax", events_until.as_str()),
     ]);
 
     let response = google_client
