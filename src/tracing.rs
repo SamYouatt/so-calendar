a79845fa5b9ef28e6d4fa0b8579d3ae393c944dd
SamYouatt <sdyouatt@gmail.com>
2024-06-19 20:23:52 +0100

    use trace level from RUST_LOG or default to INFO

--- src/tracing.rs
+++ src/tracing.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 use color_eyre::eyre::Result;
 use tracing_error::ErrorLayer;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, Layer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Layer};
 
 pub fn initialise_tracing() -> Result<()> {
     let log_dir = dirs_next::data_dir()
@@ -14,15 +14,22 @@ pub fn initialise_tracing() -> Result<()> {
     let log_file_path = log_dir.join("tracing");
     let log_file = fs::File::create(log_file_path)?;
 
+    // Try and read RUST_LOG for log level filter or default to info
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+
     let file_subscriber = tracing_subscriber::fmt::layer()
         .with_file(true)
         .with_line_number(true)
         .with_writer(log_file)
         .with_target(false)
         .with_ansi(false)
-        .with_filter(tracing_subscriber::filter::EnvFilter::from_default_env());
+        .with_filter(env_filter);
 
-    tracing_subscriber::registry().with(file_subscriber).with(ErrorLayer::default()).init();
+    tracing_subscriber::registry()
+        .with(file_subscriber)
+        .with(ErrorLayer::default())
+        .init();
 
     Ok(())
 }
