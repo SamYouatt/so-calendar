c27404b4f5d1bc601b9fe866819614a4e13d47e2
SamYouatt <sdyouatt@gmail.com>
2024-06-21 21:03:35 +0100

    add days in month into constructor

--- src/features/month_view/components/month_view_widget.rs
+++ src/features/month_view/components/month_view_widget.rs
@@ -1,15 +1,21 @@
-use crate::domain::events::{DayEvent, Event};
+use chrono::{DateTime, Local};
+
+use crate::{domain::events::{DayEvent, Event}, util::calendar_helpers::days_in_month};
 
 pub struct MonthViewWidget<'a> {
+    month: DateTime<Local>,
     events: &'a [Event],
     day_events: &'a [DayEvent],
+    days_in_month: u32,
 }
 
 impl<'a> MonthViewWidget<'a> {
-    pub fn new(events: &'a [Event], day_events: &'a [DayEvent]) -> Self {
+    pub fn new(month: DateTime<Local>, events: &'a [Event], day_events: &'a [DayEvent]) -> Self {
         Self {
+            month,
             events,
             day_events,
+            days_in_month: days_in_month(month),
         }
     }
 }
